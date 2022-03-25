b276b7edab20f8d161e7ae12c5c70661a242a0b4
qinyao <qinyao@mail.jj.cn>
2022-03-25 17:51:17 +0800

    init commit

--- src/process.py
+++ src/process.py
@@ -21,10 +21,14 @@ class Process:
         if 'STORE_NAME' != inst.opname:
             return
         inst_store = self.utils.stack_pop()
-        var_name = inst_store.argval
-        self.digraph.add_vertex(var_name)
+        value_lhs = inst_store.argval
+        self.digraph.add_vertex(value_lhs)
         inst_rhs = self.utils.stack_pop()
-        if inst_rhs.opname == 'LOAD_CONST':
+        self.process_value_rhs(inst_rhs, value_lhs)
+        
+
+    def process_value_rhs(self, inst_rhs, value_lhs):
+        if 'LOAD_' in inst_rhs.opname:
             pass
         elif inst_rhs.opname == 'IMPORT_NAME' or inst_rhs.opname == 'IMPORT_FROM':
             pass
@@ -32,15 +36,13 @@ class Process:
             value_count = inst_rhs.argval
             for i in range(value_count):
                 inst_list_value = self.utils.stack_pop()
-                vertex_out = self.process_inst(inst_list_value)
-                self.digraph.add_edge(vertex_out, var_name)
-
-
-    def process_inst(self, inst):
-        if inst.opname == 'LOAD_CONST':
+                vertex_out = self.process_value_rhs(inst_list_value)
+                self.digraph.add_edge(vertex_out, value_lhs)
+        elif inst_rhs.opname == 'BUILD_DICT':
             pass
-        elif inst.opname == 'CALL_':
+        elif inst_rhs.opname == 'CALL_FUNCTION':
             pass
-        return ''
+        elif inst_rhs.opname == 'MAKE_FUNCTION':
+            pass
 
 
